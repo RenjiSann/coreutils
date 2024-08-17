40f7fbb97238cddf6a54f20ff6cbb498ae5ac41d
Dorian Péron <dorianperon.i@gmail.com>
2024-08-17 19:37:57 +0200

    feat(checksum): extract utility functions to specific file to make logic simpler

---
+++ src/uucore/src/lib/features/checksum/utils.rs
@@ -0,0 +1,128 @@
+// This file is part of the uutils coreutils package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
+use os_display::Quotable;
+use std::{ffi::OsStr, path::Path};
+
+use crate::show_warning_caps;
+
+use super::ChecksumResult;
+
+/// Create a string version of the OsStr for display.
+pub fn get_filename_for_output(filename: &OsStr, input_is_stdin: bool) -> String {
+    if input_is_stdin {
+        "standard input"
+    } else {
+        filename.to_str().unwrap()
+    }
+    .maybe_quote()
+    .to_string()
+}
+
+/// Print diagnostic lines at the end of the processing of a checksum file.
+#[allow(clippy::comparison_chain)]
+pub fn cksum_output(res: &ChecksumResult, ignore_missing: bool, status: bool) {
+    if res.bad_format == 1 {
+        show_warning_caps!("{} line is improperly formatted", res.bad_format);
+    } else if res.bad_format > 1 {
+        show_warning_caps!("{} lines are improperly formatted", res.bad_format);
+    }
+
+    if !status {
+        if res.failed_cksum == 1 {
+            show_warning_caps!("{} computed checksum did NOT match", res.failed_cksum);
+        } else if res.failed_cksum > 1 {
+            show_warning_caps!("{} computed checksums did NOT match", res.failed_cksum);
+        }
+    }
+    if !ignore_missing {
+        if res.failed_open_file == 1 {
+            show_warning_caps!("{} listed file could not be read", res.failed_open_file);
+        } else if res.failed_open_file > 1 {
+            show_warning_caps!("{} listed files could not be read", res.failed_open_file);
+        }
+    }
+}
+
+/// Given a Path, create an escaped string version of it, where control chars
+/// are preceded with `\` backslashes.
+pub fn escape_filename(filename: &Path) -> (String, &'static str) {
+    let original = filename.as_os_str().to_string_lossy();
+    let escaped = original
+        .replace('\\', "\\\\")
+        .replace('\n', "\\n")
+        .replace('\r', "\\r");
+    let prefix = if escaped == original { "" } else { "\\" };
+    (escaped, prefix)
+}
+
+/// Revert the effect of `escape_filename`, takes a byte array in argument.
+pub fn unescape_filename(filename: &[u8]) -> (Vec<u8>, &'static str) {
+    let mut unescaped = Vec::with_capacity(filename.len());
+    let mut byte_iter = filename.iter().peekable();
+    loop {
+        let Some(byte) = byte_iter.next() else {
+            break;
+        };
+        if *byte == b'\\' {
+            match byte_iter.next() {
+                Some(b'\\') => unescaped.push(b'\\'),
+                Some(b'n') => unescaped.push(b'\n'),
+                Some(b'r') => unescaped.push(b'\r'),
+                Some(x) => {
+                    unescaped.push(b'\\');
+                    unescaped.push(*x);
+                }
+                _ => {}
+            }
+        } else {
+            unescaped.push(*byte);
+        }
+    }
+    let prefix = if unescaped == filename { "" } else { "\\" };
+    (unescaped, prefix)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_escape_filename() {
+        let (escaped, prefix) = escape_filename(Path::new("testfile.txt"));
+        assert_eq!(escaped, "testfile.txt");
+        assert_eq!(prefix, "");
+
+        let (escaped, prefix) = escape_filename(Path::new("test\nfile.txt"));
+        assert_eq!(escaped, "test\\nfile.txt");
+        assert_eq!(prefix, "\\");
+
+        let (escaped, prefix) = escape_filename(Path::new("test\rfile.txt"));
+        assert_eq!(escaped, "test\\rfile.txt");
+        assert_eq!(prefix, "\\");
+
+        let (escaped, prefix) = escape_filename(Path::new("test\\file.txt"));
+        assert_eq!(escaped, "test\\\\file.txt");
+        assert_eq!(prefix, "\\");
+    }
+
+    #[test]
+    fn test_unescape_filename() {
+        let (unescaped, prefix) = unescape_filename(b"test\\nfile.txt");
+        assert_eq!(unescaped, b"test\nfile.txt");
+        assert_eq!(prefix, "\\");
+        let (unescaped, prefix) = unescape_filename(b"test\\nfile.txt");
+        assert_eq!(unescaped, b"test\nfile.txt");
+        assert_eq!(prefix, "\\");
+
+        let (unescaped, prefix) = unescape_filename(b"test\\rfile.txt");
+        assert_eq!(unescaped, b"test\rfile.txt");
+        assert_eq!(prefix, "\\");
+
+        let (unescaped, prefix) = unescape_filename(b"test\\\\file.txt");
+        assert_eq!(unescaped, b"test\\file.txt");
+        assert_eq!(prefix, "\\");
+    }
+}
